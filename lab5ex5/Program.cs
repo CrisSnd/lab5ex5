d93922bb8916b638e1b9902cb1b981f1916da279
Cristi Sandu <sandu.cristi@yahoo.com>
2023-07-20 19:08:39 +0300

    Add project files.

---
+++ lab5ex5/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace MyApp
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            /*Program care va normaliza un sir de caractere citit de la tastatura astfel incat fiecare cuvant din sir sa inceapa cu MAJUSCULA, restul literelor fiind transformate in litere mici.*/
+
+
+            Console.WriteLine("Introduceti sirul de caractere: ");
+            string sirCaractere = Console.ReadLine();
+        
+            Console.WriteLine(MajusculeCuvinte(sirCaractere));
+        }
+
+
+        static string MajusculeCuvinte(string text)
+        {
+                
+                text = text.ToLower();
+                char[] matriceCaractere = text.ToCharArray();
+              
+                if (matriceCaractere.Length >= 1)
+                {
+                    if (char.IsLower(matriceCaractere[0]))
+                    {
+                        matriceCaractere[0] = char.ToUpper(matriceCaractere[0]);
+                    }
+                }
+                
+
+                for (int i = 1; i < matriceCaractere.Length; i++)
+                {
+                    if (matriceCaractere[i - 1] == ' ')
+                    {
+                        if (char.IsLower(matriceCaractere[i]))
+                        {
+                        matriceCaractere[i] = char.ToUpper(matriceCaractere[i]);
+                        }
+                    }
+                }
+                return new string(matriceCaractere);
+            }
+        }
+    }
+    
+
+
+    
+ 
+
+ 
